73a3407055c034f369cf4cf849a90a46cff1dd4f
USER25 <USER25@USER14-PC>
2019-10-25 16:04:18 +0900

    게이머 합계 기능 수정

--- SFBlackJack/src/com/hk/blackjack/Gamer.java
+++ SFBlackJack/src/com/hk/blackjack/Gamer.java
@@ -2,24 +2,24 @@ package com.hk.blackjack;
 
 public class Gamer {
 	protected Card[] myCards = new Card[10];
-		
+	protected int sum = 0;	
+	
 	//카드 주소값을 외부로부터 받아서 myCards배열에 차곡차곡
 	//저장한다.
 	public void addCard (Card card) {
 		for(int i=0; i<myCards.length; i++) {
 			if(myCards[i] == null) {
 				myCards[i] = card;
+				sum += card.value;
 				return;
 			}
 		}
 	}
 	
-	public void openCards() {
-		int sum = 0;
+	public void openCards() {	
 		for(Card c : myCards) {
 			if(c != null) {
-				System.out.println(c);
-				sum += c.value;
+				System.out.println(c);				
 			}			
 		}
 		System.out.printf("sum : %d\n", sum);
